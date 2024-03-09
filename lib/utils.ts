353280f843b61761d2ea69651fd9dbaa457498fa
totto2727 <kaihatu.totto2727@gmail.com>
2024-03-09 14:35:53 +0900

    fix: modified from default import to named import

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
-import validator from 'validator';
+import { isURL } from 'validator';
 import type { CustomMetaTags, ValidatorSettings, OpenGraphScraperOptions } from './types';
 
 export const defaultUrlValidatorSettings = {
@@ -25,7 +25,7 @@ export const defaultUrlValidatorSettings = {
  *
  */
 export function isUrlValid(url: string, urlValidatorSettings: ValidatorSettings): boolean {
-  return typeof url === 'string' && url.length > 0 && validator.isURL(url, urlValidatorSettings);
+  return typeof url === 'string' && url.length > 0 && isURL(url, urlValidatorSettings);
 }
 
 /**
